4dd71dcff1deaee62944e7dac3ff49615d522645
Brett Jay Packard <brettjayp@gmail.com>
2019-10-29 18:17:57 -0700

    initialized repository, added essentials

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 201n16_about-me
 Brett Packard, CodeFellows seattle-201n16, lab 02 03 04 05b, About Me
+
+2019 10 29 @ 1815
+- Created repository
+- Added initial documents
---
+++ js/app.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unused-vars */
+'use strict';
+
+
+
+// end of line, do not proceed
