c671b1a60c37809e3822fe414d71113c08c1fa33
Brett Jay Packard <brettjayp@gmail.com>
2019-10-29 23:36:45 -0700

    js prompts 5 y/n questions

--- js/app.js
+++ js/app.js
@@ -3,6 +3,19 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
+alert('Hello, play game, about me');
+console.log('user clicked ok on hello');
+
+const guessThreeDee = prompt('I like 3D printing, and have a couple printers. Do I have a Prusa i3 MK3S?').toLowerCase();
+console.log(`${guessThreeDee} : input for guessThreeDee`);
+const guessScubaDiving = prompt('I enjoy scuba diving. Do you think I\'m a divemaster?').toLowerCase();
+console.log(`${guessScubaDiving} : input for guessScubaDiving`);
+const guessTechJob = prompt('I work in technology and enjoy it. Have I worked in a data center?').toLowerCase();
+console.log(`${guessTechJob} : input for guessTechJob`);
+const guessMOS = prompt('I served in the military. Was I a grunt in the Army?').toLowerCase();
+console.log(`${guessMOS} : input for guessMOS`);
+const guessDog = prompt('I have one dog. Is she a German shepherd and a husky?').toLowerCase();
+console.log(`${guessDog} : input for guessDog`);
 
 
 // end of line, do not proceed
