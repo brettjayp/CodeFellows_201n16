ac59176166176348b640dab4870d7680995f99d8
Brett Jay Packard <brettjayp@gmail.com>
2019-10-29 23:59:50 -0700

    iterative progress

--- js/app.js
+++ js/app.js
@@ -3,18 +3,27 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
+// declare global variables
+var guessThreeDee, guessScubaDiving, guessTechJob, guessMOS, guessDog = '';
+
+// establish y/n function
+function guessMe (variable, answer, question, ifYes, ifNo) {
+    variable = prompt(question).toLowerCase();
+    console.log(`${variable} : user input for `);
+}
+
 alert('Hello, play game, about me');
 console.log('user clicked ok on hello');
 
-const guessThreeDee = prompt('I like 3D printing, and have a couple printers. Do I have a Prusa i3 MK3S?').toLowerCase();
+guessThreeDee = prompt('I like 3D printing, and have a couple printers. Do I have a Prusa i3 MK3S?').toLowerCase();
 console.log(`${guessThreeDee} : input for guessThreeDee`);
-const guessScubaDiving = prompt('I enjoy scuba diving. Do you think I\'m a divemaster?').toLowerCase();
+guessScubaDiving = prompt('I enjoy scuba diving. Do you think I\'m a divemaster?').toLowerCase();
 console.log(`${guessScubaDiving} : input for guessScubaDiving`);
-const guessTechJob = prompt('I work in technology and enjoy it. Have I worked in a data center?').toLowerCase();
+guessTechJob = prompt('I work in technology and enjoy it. Have I worked in a data center?').toLowerCase();
 console.log(`${guessTechJob} : input for guessTechJob`);
-const guessMOS = prompt('I served in the military. Was I a grunt in the Army?').toLowerCase();
+guessMOS = prompt('I served in the military. Was I a grunt in the Army?').toLowerCase();
 console.log(`${guessMOS} : input for guessMOS`);
-const guessDog = prompt('I have one dog. Is she a German shepherd and a husky?').toLowerCase();
+guessDog = prompt('I have one dog. Is she a German shepherd and a husky?').toLowerCase();
 console.log(`${guessDog} : input for guessDog`);
 
 
