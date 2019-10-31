f61e1a5afd4b4e84a7177175fa7b93ea82e64821
WondwosenTsige <wondneh@gmail.com>
2019-10-30 19:17:27 -0700

    submitting assignment

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 'use strict';
 
-console.log('proof of life');
+//console.log('proof of life');
 
-var userLike= prompt ("Do you like stranger things?");
-//console.log('userLike: ', userLike);
+var userInfo= prompt ("What is your name?");
+//console.log('userInfo: ' , userInfo);
 
-userLike= userLike.toLowerCase();
+alert("welcome " + userInfo);
 
-console.log('userLike: ' , userLike);
+prompt ("Are you ok with this coding thing?");
 
-if(userLike.toLowerCase()==='yes') {
-    console.log ('I like it too');
+userInfo= userInfo.toLowerCase();
+//userInfo= userInfo.toUpperCase();
+
+//console.log('userInfo: ' , userInfo);
+
+if(userInfo.toLowerCase()==='yes' 
+|| "y") 
+{
+    alert ("Great this guys is doing good");
+    //console.log ('I like it too');
 }
+
+
 else {
-    console.log('you should check it out');
+    //console.log('you should check it out');
+    alert ("Do not worry. we are on the same path");
+
+
 }
+
+alert("Thank you " + userInfo + " for your time. Good bye");
