da199e576c08fee77e509be66256b5e67a45e403
Brett Jay Packard <brettjayp@gmail.com>
2019-11-11 21:30:45 -0800

    PUSH! BATTERY ALMOST DEAD!

--- js/app.js
+++ js/app.js
@@ -10,71 +10,46 @@ const elDubai = document.getElementById('shopDubai');
 const elParis = document.getElementById('shopParis');
 const elLima = document.getElementById('shopLima');
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+let allShops = [];
 
-// shop object literals
-let shopFirstAndPike = {
-    parent: elFirstAndPike,
-    title: 'Location: Seattle',
-    minCustPerHour: 23,
-    maxCustPerHour: 65,
-    avgCookiesPerCustomer: 6.3,
-    customersPerHour: [],
-    cookiesPerHour: [],
-    ttlCookiesForDay: 0,
-    
-    render: function() {
-        addEl('lh', this.title, this.parent);
-        for(let i = 0; i < hours.length; ++i) {
-            this.customersPerHour.push(randomMinMax(this.minCustPerHour, this.maxCustPerHour));
-            this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
-            this.ttlCookiesForDay = this.ttlCookiesForDay + this.cookiesPerHour[i];
-            addEl('li', `${hours[i]}, Cookies: ${this.cookiesPerHour[i]}`, this.parent);
-        }
-        addEl('li', `Total Sales: ${this.ttlCookiesForDay}`, this.parent);
+// shop constructor
+function Shop (name, parent, min, max, avg) {
+    this.shopName = name;
+    this.parent = parent;
+    this.minCustPerHour = min;
+    this.maxCustPerHour = max;
+    this.avgCookiesPerCustomer = avg;
+    this.customersPerHour = [];
+    this.cookiesPerHour = [];
+    this.ttlCookiesPerDay = [];
+
+    allShops.push(this);
+}
+
+Shop.prototype.doCalculations = function(){
+    for (let i = 0; i < hours.length; ++i){
+        this.customersPerHour.push(randomMinMax(this.minCustPerHour, this.maxCustPerHour));
+        this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
+        this.ttlCookiesPerDay = this.ttlCookiesPerDay + this.cookiesPerHour[i];
     }
 };
-let shopTokyo = {
-    parent: elTokyo,
-    title: 'Location: Tokyo',
-    minCustPerHour: 3,
-    maxCustPerHour: 24,
-    avgCookiesPerCustomer: 1.2,
-    customersPerHour: [],
-    cookiesPerHour: [],
-    ttlCookiesForDay: 0,
-
-    render: function() {
-        addEl('lh', this.title, this.parent);
-        for(let i = 0; i < hours.length; ++i) {
-            this.customersPerHour.push(randomMinMax(this.minCustPerHour, this.maxCustPerHour));
-            this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
-            this.ttlCookiesForDay = this.ttlCookiesForDay + this.cookiesPerHour[i];
-            addEl('li', `${hours[i]}, Cookies: ${this.cookiesPerHour[i]}`, this.parent);
-        }
-        addEl('li', `Total Sales: ${this.ttlCookiesForDay}`, this.parent);
+Shop.prototype.renderList = function(){
+    for (let i = 0; i < hours.length; ++i) {
+        addEl('li', `${hours[i]}, Cookies: ${this.cookiesPerHour[i]}`, this.parent);
     }
-};
-let shopDubai = {
-    parent: elDubai,
-    title: 'Location: Dubai',
-    minCustPerHour: 11,
-    maxCustPerHour: 38,
-    avgCookiesPerCustomer: 3.7,
-    customersPerHour: [],
-    cookiesPerHour: [],
-    ttlCookiesForDay: 0,
+    addEl('li', `Total Sales: ${this.ttlCookiesPerDay}`, this.parent);
+}
 
-    render: function() {
-        addEl('lh', this.title, this.parent);
-        for(let i = 0; i < hours.length; ++i) {
-            this.customersPerHour.push(randomMinMax(this.minCustPerHour, this.maxCustPerHour));
-            this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
-            this.ttlCookiesForDay = this.ttlCookiesForDay + this.cookiesPerHour[i];
-            addEl('li', `${hours[i]}, Cookies: ${this.cookiesPerHour[i]}`, this.parent);
-        }
-        addEl('li', `Total Sales: ${this.ttlCookiesForDay}`, this.parent);
-    }
-};
+new Shop ('Seattle', elFirstAndPike, 23, 65, 6.3);
+new Shop ('Tokyo', elTokyo, 3, 24, 1.2);
+new Shop ('Dubai', elDubai, 11, 38, 3.7);
+new Shop ('Paris', elParis, 20, 38, 2.3);
+
+for (let i = 0; i < allShops.length; ++i){
+    allShops[i].doCalculations();
+    allShops[i].renderList();
+}
+
 let shopParis = {
     parent: elParis,
     title: 'Location: Paris',
@@ -96,6 +71,30 @@ let shopParis = {
         addEl('li', `Total Sales: ${this.ttlCookiesForDay}`, this.parent);
     }
 };
+
+
+// shop object literals
+// let shopFirstAndPike = {
+//     parent: elFirstAndPike,
+//     title: 'Location: Seattle',
+//     minCustPerHour: 23,
+//     maxCustPerHour: 65,
+//     avgCookiesPerCustomer: 6.3,
+//     customersPerHour: [],
+//     cookiesPerHour: [],
+//     ttlCookiesForDay: 0,
+    
+//     render: function() {
+//         addEl('lh', this.title, this.parent);
+//         for(let i = 0; i < hours.length; ++i) {
+//             this.customersPerHour.push(randomMinMax(this.minCustPerHour, this.maxCustPerHour));
+//             this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
+//             this.ttlCookiesForDay = this.ttlCookiesForDay + this.cookiesPerHour[i];
+//             addEl('li', `${hours[i]}, Cookies: ${this.cookiesPerHour[i]}`, this.parent);
+//         }
+//         addEl('li', `Total Sales: ${this.ttlCookiesPerDay}`, this.parent);
+//     }
+// };
 let shopLima = {
     parent: elLima,
     title: 'Location: Lima',
