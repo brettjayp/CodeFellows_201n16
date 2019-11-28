43f846b13f14b58afa437c73f21e47d15b13b8f2
Brett Jay Packard <brettjayp@gmail.com>
2019-11-27 20:08:17 -0800

    init setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 201n16_bus-mall
+
+2019 11 27
+- init of new repo, adding basics
+- 
---
+++ js/app.js
@@ -0,0 +1,3 @@
+/* eslint-disable indent */ /* eslint-disable no-trailing-spaces */ /* eslint-disable no-unused-vars */ /* eslint-disable no-multi-spaces */
+'use strict';
+
