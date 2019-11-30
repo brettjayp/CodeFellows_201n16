b7f685c0a69585e527a1459036657435a6422a98
Brett Jay Packard <brettjayp@gmail.com>
2019-11-29 23:53:29 -0800

    general cleanup, update readme

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # 201n16_bus-mall
 
+- Broken down, goal of first lab (lab11) is:
+  - create a page that displays three products at a time from a catalog at random
+  - track the frequency each object is displayed
+  - user must select which product they'd most likely purchase
+  - track the frequency each object gets selected
+  - display the three product lineup 25 times to the user
+  - upon 25 interactions from the user, prevent further interaction
+  - upon 25 interactions from the user, run some calculations, and render some results to the page
+  - apply some appropriate styling
+
 2019 11 27
 - init of new repo, adding basics
 - new branch: lab11
+
+2019 11 29
+- HTML: Basic setup, grey on grey etc, simple section and div to contain elements
+- CSS: Styles to match basic HTML page, and for elements to be created
+- JS: Added some basic helper functions, random number generator and add new element
+- JS: Constructor function to take in products, push to catalog[]
+- JS: Functions created to render a product to page (dependent on position), select a product at random without repeats or duplicates and render to the page (dependent on position), and render three random products without repeats or duplicates.
+- JS: When new products are rendered to the page, we clear previous objects from the apge, we track the previous three images to prevent repeats, we increment the display count of products being rendered, we track the images as we make them to prevent duplicates being shown, and we update a table in the console displaying our catalog[] data.
+- 
--- js/app.js
+++ js/app.js
@@ -10,6 +10,21 @@ let catalog = [];
 let lastThree = [-1, -1, -1];
 let newThree = [-1, -1, -1];
 
+// Here we build out catalog of products
+new Product('bag', 'bag.jpg', 'An R2-D2 suitcase');
+new Product('banana', 'banana.jpg', 'A banana slicer');
+new Product('bathroom', 'bathroom.jpg', 'A tablet mount on a toilet paper holder');
+new Product('boots', 'boots.jpg', 'A pair of pointless rain boots with open toes');
+new Product('breakfast', 'breakfast.jpg', 'An all-in-one breakfast machine');
+new Product('bubblegum', 'bubblegum.jpg', 'Bubblegum designed to look like meatballs');
+new Product('chair', 'chair.jpg', 'A very uncomfortable tall chair');
+new Product('cthulhu', 'cthulhu.jpg', 'An action figure of Cthulhu');
+
+// // // CALL OUR FEATURE FUNCTION // // //
+showThree();
+// // // CALL OUR FEATURE FUNCTION // // //
+
+// PIMARY CONSTRUCTOR FUNCTION: This is our constructor function to build new products for the main feature. They each will be pushed to the catalog[] array.
 function Product(title, source, alt){
   this.title = title;
   this.alt = alt;
@@ -20,20 +35,9 @@ function Product(title, source, alt){
 
   catalog.push(this);
 }
-
-new Product('bag', 'bag.jpg', 'An R2-D2 suitcase');
-new Product('banana', 'banana.jpg', 'A banana slicer');
-new Product('bathroom', 'bathroom.jpg', 'A tablet mount on a toilet paper holder');
-new Product('boots', 'boots.jpg', 'A pair of pointless rain boots with open toes');
-new Product('breakfast', 'breakfast.jpg', 'An all-in-one breakfast machine');
-new Product('bubblegum', 'bubblegum.jpg', 'Bubblegum designed to look like meatballs');
-new Product('chair', 'chair.jpg', 'A very uncomfortable tall chair');
-new Product('cthulhu', 'cthulhu.jpg', 'An action figure of Cthulhu');
-
+// PRIMARY FUNCTION: Show three new products to the page. They will not repeat any of the previous round's images, and there will be no duplicates displayed. We also reset the newThree[] array and overwrite the lastThree[] array for tracking and to assist in avoiding repeats and duplicates.
+// For now, we're also displaying a table in the console of our catalog[] items for visibility in tracking. This will clear and update every time we show new products.
 function showThree(){
-  // Every time we show three new images, we need to know which three were just displayed, in order to avoid immediate repeats.
-  // Every time we show three new images, we need to keep track of which ones we're displaying, in order to not display duplicates.
-  // We could write the three images to 'lastThree' at the end of each round, but then we lose the historical data for debugging, so we write it at the beginning.
   lastThree = [newThree[0], newThree[1], newThree[2]];
   newThree = [-1, -1, -1];
 
@@ -44,7 +48,7 @@ function showThree(){
   console.clear();
   console.table(catalog);
 }
-
+// PRIMARY FUNCTION: By using renderProduct() and randomMinMax(), select a random product and render it to the page, dependent on position (0, 1, 2). Also set the current newThree[] position to reflect which image is being shown, and increment that image's displayCount value.
 function randomProduct(position){
   let integer = randomMinMax(0, catalog.length);
   if(position === 0){
@@ -63,12 +67,8 @@ function randomProduct(position){
   newThree[position] = integer;
   renderProduct(catalog[integer], position);
   ++catalog[integer].displayCount;
-
 }
-
-showThree();
-
-// helper functions
+// PRIMARY FUNCTION: Render a product image to the page, dependent on position (0, 1, 2).
 function renderProduct(source, position){
   let newImg = document.createElement('img');
   if(position === 0){
@@ -97,6 +97,7 @@ function renderProduct(source, position){
   }
   return newImg;
 }
+// HELPER FUNCTION: Add a new element.
 function addEl(element, content, parent){
   let newElement = document.createElement(element);
   if(content){
@@ -107,7 +108,7 @@ function addEl(element, content, parent){
   parent.appendChild(newElement);
   return newElement;
 }
+// HELPER FUNCTION: Return a random integer within given parameters.
 function randomMinMax(min, max){
-  // return Math.floor((Math.random() * ((max - min) + 1)) + min);
-  return Math.floor((Math.random() * ((max - min))) + min);
+  return Math.floor((Math.random() * ((max - min))) + min); // could include a '+1' but it sometimes breaks here
 }
