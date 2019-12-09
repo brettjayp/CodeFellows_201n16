562e48d9097653699bf48c8e409bfd6f68fd3397
Brett Jay Packard <brettjayp@gmail.com>
2019-12-08 23:46:29 -0800

    removed linter disablers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable indent */ /* eslint-disable no-trailing-spaces */ /* eslint-disable no-unused-vars */ /* eslint-disable no-multi-spaces */
 'use strict';
 
 // function and elements to alert user that a page is not finished when clicking a link
@@ -6,8 +5,8 @@ document.getElementById('cutter').addEventListener('click', clickNoLink);
 document.getElementById('fish').addEventListener('click', clickNoLink);
 document.getElementById('shirt').addEventListener('click', clickNoLink);
 document.getElementById('frosted').addEventListener('click', clickNoLink);
-function clickNoLink(event){
-  alert('Sorry, that page is still under construction.');
+function clickNoLink(){
+    alert('Sorry, that page is still under construction.');
 }
 
 // end of line, do not proceed
--- js/sales.js
+++ js/sales.js
@@ -1,68 +1,67 @@
-/* eslint-disable indent */ /* eslint-disable no-trailing-spaces */ /* eslint-disable no-unused-vars */ /* eslint-disable no-multi-spaces */
 'use strict';
 // constants, elements from HTML
 const elCookieTable = document.getElementById('cookieTable');
 const elEmployeeTable = document.getElementById('employeeTable');
 const elFormNew = document.getElementById('formNew');
-const elButtonSubmit = document.getElementById('buttonSubmit');
+// const elButtonSubmit = document.getElementById('buttonSubmit');
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 const adjustments = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
 let allShops = [];
 // shop constructor and prototypes/methods
 function Shop (name, min, max, avg){
-  this.shopName = name;
-  this.minCustPerHour = min;
-  this.maxCustPerHour = max;
-  this.avgCookiesPerCustomer = avg;
-  this.customersPerHour = [];
-  this.cookiesPerHour = [];
-  this.ttlCookiesPerDay = 0;
-  this.tossersPerHour = [];
-  this.ttlTosserHrs = 0;
-  allShops.push(this);
+    this.shopName = name;
+    this.minCustPerHour = min;
+    this.maxCustPerHour = max;
+    this.avgCookiesPerCustomer = avg;
+    this.customersPerHour = [];
+    this.cookiesPerHour = [];
+    this.ttlCookiesPerDay = 0;
+    this.tossersPerHour = [];
+    this.ttlTosserHrs = 0;
+    allShops.push(this);
 }
 // calculate and store customersPerHour, cookiesPerHour, and ttlCookiesPerDay
 Shop.prototype.doCalculations = function(){
-  while (this.cookiesPerHour.length < hours.length){
-    for (let i = 0; i < hours.length; ++i){
-      this.customersPerHour.push((randomMinMax(this.minCustPerHour, this.maxCustPerHour)));// * adjustments[i]);
-      this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
-      // I have NO IDEA why, but I occasionally get 0's returned after my Math logic. So to force a value, even it it's below the theoretical minimum, I'm forcing the code to keep running the above two lines, replacing the previous values of course, until I get a value > 0. I use a while loop to accomplish this.
-      while (this.cookiesPerHour[i] === 0){
-        this.customersPerHour[i] = (randomMinMax(this.minCustPerHour, this.maxCustPerHour)) * adjustments[i];
-        this.cookiesPerHour [i] = Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer);
-      }
-      this.ttlCookiesPerDay = this.ttlCookiesPerDay + this.cookiesPerHour[i];
+    while (this.cookiesPerHour.length < hours.length){
+        for (let i = 0; i < hours.length; ++i){
+            this.customersPerHour.push((randomMinMax(this.minCustPerHour, this.maxCustPerHour)));// * adjustments[i]);
+            this.cookiesPerHour.push(Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer));
+            // I have NO IDEA why, but I occasionally get 0's returned after my Math logic. So to force a value, even it it's below the theoretical minimum, I'm forcing the code to keep running the above two lines, replacing the previous values of course, until I get a value > 0. I use a while loop to accomplish this.
+            while (this.cookiesPerHour[i] === 0){
+                this.customersPerHour[i] = (randomMinMax(this.minCustPerHour, this.maxCustPerHour)) * adjustments[i];
+                this.cookiesPerHour [i] = Math.floor(this.customersPerHour[i] * this.avgCookiesPerCustomer);
+            }
+            this.ttlCookiesPerDay = this.ttlCookiesPerDay + this.cookiesPerHour[i];
+        }
     }
-  }
 };
 // renders data to the cookies table (shop name, cookies per hour, cookies per day)
 Shop.prototype.renderCookies = function(){
-  let row = addEl('tr', false, elCookieTable);
-  addEl('th', this.shopName, row);
-  for (let i = 0; i < hours.length; ++i){
-    addEl('td', this.cookiesPerHour[i], row);
-  }
-  addEl('td',this.ttlCookiesPerDay, row);
+    let row = addEl('tr', false, elCookieTable);
+    addEl('th', this.shopName, row);
+    for (let i = 0; i < hours.length; ++i){
+        addEl('td', this.cookiesPerHour[i], row);
+    }
+    addEl('td',this.ttlCookiesPerDay, row);
 };
 // calculates and stores how many tossers are needed per hour, with a minimum of two tossers
 Shop.prototype.makeTossers = function(){
-  while (this.tossersPerHour.length < hours.length){
-    for(let i = 0; i < this.cookiesPerHour.length; ++i){
-      this.tossersPerHour.push(Math.ceil(this.cookiesPerHour[i] / 20));
-      while(this.tossersPerHour[i] < 2){this.tossersPerHour[i]++;}
-      this.ttlTosserHrs += this.tossersPerHour[i];
+    while (this.tossersPerHour.length < hours.length){
+        for(let i = 0; i < this.cookiesPerHour.length; ++i){
+            this.tossersPerHour.push(Math.ceil(this.cookiesPerHour[i] / 20));
+            while(this.tossersPerHour[i] < 2){this.tossersPerHour[i]++;}
+            this.ttlTosserHrs += this.tossersPerHour[i];
+        }
     }
-  }
 };
 // renders data to the tossers table (shop name, tossers per hour)
 Shop.prototype.renderTossers = function(){
-  let row = addEl('tr', false, elEmployeeTable);
-  addEl('th', this.shopName, row);
-  for(let i = 0; i < hours.length; ++i){
-    addEl('td', this.tossersPerHour[i], row);
-  }
-  addEl('td', this.ttlTosserHrs, row);
+    let row = addEl('tr', false, elEmployeeTable);
+    addEl('th', this.shopName, row);
+    for(let i = 0; i < hours.length; ++i){
+        addEl('td', this.tossersPerHour[i], row);
+    }
+    addEl('td', this.ttlTosserHrs, row);
 };
 // creating persistent shops
 new Shop ('Seattle', 23, 65, 6.3);
@@ -72,74 +71,74 @@ new Shop ('Paris', 20, 38, 2.3);
 new Shop ('Lima', 2, 16, 4.6);
 // do all of cookie table, and render to the page
 function renderCookieTable(){
-  let rowHours = addEl('thead', false, elCookieTable);
-  addEl('th', false, rowHours);
-  for (let i = 0; i < hours.length; ++i){
-    addEl('th', hours[i], rowHours);
-  }
-  addEl('th', 'Total', rowHours);
-  for (let i = 0; i < allShops.length; ++i){
-    allShops[i].doCalculations();
-    allShops[i].renderCookies();
-  }
-  let rowTotals = addEl('thead', false, elCookieTable);
-  addEl('th', 'Total', rowTotals);
-  for (let i = 0; i < hours.length; ++i){
-    let ttlCookiesPerHour = 0;
-    for (let j = 0; j < allShops.length; ++j){
-      ttlCookiesPerHour += allShops[j].cookiesPerHour[i];
+    let rowHours = addEl('thead', false, elCookieTable);
+    addEl('th', false, rowHours);
+    for (let i = 0; i < hours.length; ++i){
+        addEl('th', hours[i], rowHours);
     }
-    addEl('td', ttlCookiesPerHour, rowTotals);
-  }
-  let totalAll = 0;
-  for (let i = 0; i < allShops.length ; ++i){
-    totalAll += allShops[i].ttlCookiesPerDay;
-  }
-  addEl('th', totalAll, rowTotals);
+    addEl('th', 'Total', rowHours);
+    for (let i = 0; i < allShops.length; ++i){
+        allShops[i].doCalculations();
+        allShops[i].renderCookies();
+    }
+    let rowTotals = addEl('thead', false, elCookieTable);
+    addEl('th', 'Total', rowTotals);
+    for (let i = 0; i < hours.length; ++i){
+        let ttlCookiesPerHour = 0;
+        for (let j = 0; j < allShops.length; ++j){
+            ttlCookiesPerHour += allShops[j].cookiesPerHour[i];
+        }
+        addEl('td', ttlCookiesPerHour, rowTotals);
+    }
+    let totalAll = 0;
+    for (let i = 0; i < allShops.length ; ++i){
+        totalAll += allShops[i].ttlCookiesPerDay;
+    }
+    addEl('th', totalAll, rowTotals);
 }
 // do all of employee table, and render to the page
 function renderEmployeeTable(){
-  let rowHours = addEl('thead', false, elEmployeeTable);
-  addEl('th', false, rowHours);
-  for (let i = 0; i < hours.length; ++i){
-    addEl('th', hours[i], rowHours);
-  }
-  addEl('th', 'Total', rowHours);
-  for(let i = 0; i < allShops.length; ++i){
-    allShops[i].makeTossers();
-    allShops[i].renderTossers();
-  }
-  let rowTotals = addEl('thead', false, elEmployeeTable);
-  addEl('th', 'Total', rowTotals);
-  for(let i = 0; i < hours.length; ++i){
-    let ttlTossersPerHour = 0;
-    for (let j = 0; j < allShops.length; ++j){
-      ttlTossersPerHour += allShops[j].tossersPerHour[i];
+    let rowHours = addEl('thead', false, elEmployeeTable);
+    addEl('th', false, rowHours);
+    for (let i = 0; i < hours.length; ++i){
+        addEl('th', hours[i], rowHours);
     }
-    addEl('td', ttlTossersPerHour, rowTotals);
-  }
-  let totalAll = 0;
-  for (let i = 0; i < allShops.length; ++i){
-    totalAll += allShops[i].ttlTosserHrs;
-  }
-  addEl('th', totalAll, rowTotals);
+    addEl('th', 'Total', rowHours);
+    for(let i = 0; i < allShops.length; ++i){
+        allShops[i].makeTossers();
+        allShops[i].renderTossers();
+    }
+    let rowTotals = addEl('thead', false, elEmployeeTable);
+    addEl('th', 'Total', rowTotals);
+    for(let i = 0; i < hours.length; ++i){
+        let ttlTossersPerHour = 0;
+        for (let j = 0; j < allShops.length; ++j){
+            ttlTossersPerHour += allShops[j].tossersPerHour[i];
+        }
+        addEl('td', ttlTossersPerHour, rowTotals);
+    }
+    let totalAll = 0;
+    for (let i = 0; i < allShops.length; ++i){
+        totalAll += allShops[i].ttlTosserHrs;
+    }
+    addEl('th', totalAll, rowTotals);
 }
 // creates a NEW location from form input
 function createNew(event){
-  event.preventDefault();
-  document.getElementsByTagName('table')[0].innerHTML = '';
-  document.getElementsByTagName('table')[1].innerHTML = '';
-  let name = event.target.name.value;
-  let min = event.target.min.value;
-  let max = event.target.max.value;
-  let avg = event.target.avg.value;
-  new Shop(name,min,max,avg);
-  renderCookieTable();
-  renderEmployeeTable();
-  // elFormNew.reset();
-  if(allShops.length > 10){
-    elFormNew.innerHTML = 'Wow, that\'s a lot of shops!';
-  }
+    event.preventDefault();
+    document.getElementsByTagName('table')[0].innerHTML = '';
+    document.getElementsByTagName('table')[1].innerHTML = '';
+    let name = event.target.name.value;
+    let min = event.target.min.value;
+    let max = event.target.max.value;
+    let avg = event.target.avg.value;
+    new Shop(name,min,max,avg);
+    renderCookieTable();
+    renderEmployeeTable();
+    // elFormNew.reset();
+    if(allShops.length > 10){
+        elFormNew.innerHTML = 'Wow, that\'s a lot of shops!';
+    }
 }
 elFormNew.addEventListener('submit', createNew);
 
@@ -148,18 +147,18 @@ renderCookieTable();
 renderEmployeeTable();
 // helper function, add a new element
 function addEl(element, content, parent){
-  let newElement = document.createElement(element);
-  if(content){
-    let newContent = document.createTextNode(content);
-    newElement.appendChild(newContent);
+    let newElement = document.createElement(element);
+    if(content){
+        let newContent = document.createTextNode(content);
+        newElement.appendChild(newContent);
     // newElement.textContent = content;
-  }
-  parent.appendChild(newElement);
-  return newElement;
-} 
+    }
+    parent.appendChild(newElement);
+    return newElement;
+}
 // helper function, return random whole number based on min and max parameters
 function randomMinMax(min, max){
-  return Math.floor((Math.random() * (max - min)) + min);
+    return Math.floor((Math.random() * (max - min)) + min);
 }
 
 // End of line, do not proceed
